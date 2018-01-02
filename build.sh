7ee2e305c935bf33ac401604600dff67e1522224
ljx <cleocn@qq.com>
2018-01-02 19:22:52 +0800

    conf目录修改

--- build.sh
+++ build.sh
@@ -4,8 +4,6 @@ cd /Users/ljx/Downloads/新dede168/整站源码/易企秀V12.9源码/易企秀12
 docker build -f /Users/ljx/code/docker-eqx/php.Dockerfile -t cleocn/php:5.3-code .
 
 
-docker-machine  scp -r ./conf/   tx2:/data/h5editor/conf/
-
 docker login --username=100000444071 ccr.ccs.tencentyun.com
 
 docker pull ccr.ccs.tencentyun.com/cleocn/nginx:1.9
@@ -13,10 +11,10 @@ docker pull ccr.ccs.tencentyun.com/cleocn/php:5.3
 
 
 #140
-docker-machine ssh tx2 mkdir -p  {/data/h5/conf/,/data/h5/phptmp,/data/h5/Uploads,/data/h5/src}
-
-docker-machine  scp -r ./Application/Common/conf/   tx2:/data/h5/conf/
+docker-machine ssh tx2 mkdir -p  {/data/h5/Conf/,/data/h5/phptmp,/data/h5/Uploads,/data/h5/src}
 
 docker-machine  scp -r ./Uploads/   tx2:/data/h5/Uploads/
+
+docker-machine  scp -r ./Application/Common/Conf/   tx2:/data/h5/
 
 
